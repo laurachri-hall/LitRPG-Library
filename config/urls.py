c8fc19a88af7bbd48896151b6e415bada559bcb1
Laura Hall <laura@allthehalls.com>
2024-11-25 12:28:10 +0000

    Fix: removed unused import from config urls

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from review.views import my_review
 
 urlpatterns = [
     path('admin/', admin.site.urls),
