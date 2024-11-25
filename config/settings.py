e9057d421ec0142eeefd9cc9732619a21b535cc6
Laura Hall <laura@allthehalls.com>
2024-11-25 10:49:34 +0000

    Fix: removed trailing backslash from url in allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-laurachriha-litrpglibra-oilbcmmfwss.ws.codeinstitute-ide.net',
-    'litrpg-library-2e24401b712e.herokuapp.com/'
+    'litrpg-library-2e24401b712e.herokuapp.com',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
