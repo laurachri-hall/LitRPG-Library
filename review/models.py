c49668b509209fa54ce3d59b0e5b4385c15d0512
Laura Hall <laura@allthehalls.com>
2024-11-27 14:18:13 +0000

    Add: Comment model to review app

--- review/models.py
+++ review/models.py
@@ -29,3 +29,13 @@ class Review(models.Model):
    
     def __str__(self):
         return f'Review for {self.book.title}'
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.review.book.title} review'
