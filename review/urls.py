c423c0c6d546873aaff7847675efdad559518402
Laura Hall <laura@allthehalls.com>
2024-11-28 12:05:15 +0000

    Add: individual review page created, review_detail.html and collected new static files

--- review/urls.py
+++ review/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
+    path('<slug:slug>/', views.review_detail, name='review_detail'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.views import generic
 from .models import Review
@@ -13,3 +13,17 @@ class ReviewList(generic.ListView):
     queryset = Review.objects.filter(status=1)
     template_name = "review/index.html"
     paginate_by = 6
+
+    """
+    Displays individual review
+    """
+
+def review_detail(request, slug):
+    queryset = Review.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "review/review_detail.html",
+        {"review": review},
+    )
