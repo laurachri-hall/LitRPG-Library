64037aa14cf3f24aa13ae870529fd8532d44f6d6
Laura Hall <laura@allthehalls.com>
2024-12-04 14:27:00 +0000

    Add: new index.html, old one changed to review_list.html

--- review/urls.py
+++ review/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
     path('add_review/', views.add_review, name='add_review'),
     path('add_book/', views.add_book, name='add_book'),
+    path('review/review_list', views.review_detail, name='review_list'),
     path('review/<slug:slug>/', views.review_detail, name='review_detail'),
     path('review/<slug:slug>/edit/', views.review_edit, name='review_edit'),
     path('review/<slug:slug>/delete/', views.review_delete, name='review_delete'), 
