58b882c525db8b20491ccab0eae2a668c6612255
Laura Hall <laura@allthehalls.com>
2024-12-07 07:06:54 +0000

    Fix: TemplateView needed to be imported in urls

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 
 
